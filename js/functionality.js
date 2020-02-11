9258bc248ca2cb53a00cf831429e2af6f7ddafb0
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2020-02-10 20:28:21 -0500

    Updated logo to all pages.

---
+++ js/functionality.js
@@ -0,0 +1,12 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// Auto-updating Copyright Year
+document.getElementById("footer").innerHTML="Copyright&copy; "+(new Date().getFullYear())+", KingDish, Inc.";
